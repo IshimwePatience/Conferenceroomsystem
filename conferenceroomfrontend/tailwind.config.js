e41a30b017c9ba718ce0e6bf6a99dcaafaa0c43c
IshimwePatience <ishimweaugstin12@gmail.com>
2025-07-24 11:45:04 +0200

    initial today2

--- conferenceroomfrontend/tailwind.config.js
+++ conferenceroomfrontend/tailwind.config.js
@@ -7,16 +7,30 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
       screens: {
-        sm: "100%",
-        md: "100%",
-        lg: "900px",
-        xl: "1200px",
+        sm: "640px",
+        md: "768px", 
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+      }
+    },
   },
   plugins: [],
 }
-
